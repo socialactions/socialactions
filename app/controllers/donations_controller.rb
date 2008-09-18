329db8f6c46d5871c6f54ebb870b205102f63d5b
Josh Crawford <josh@brattcollective.com>
2008-09-18 18:57:54 -0400

    make sure we're using ssl when we should be and don't verify the cert (test site has a bogus cert)

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -24,34 +24,40 @@ class DonationsController < ApplicationController
     # if @donation.valid? && @donation.process
 
     # TODO: encapsulate this in controller or model method...
-    res = Net::HTTP.post_form(URI.parse('http://qa4.networkforgood.org/PartnerDonationService/DonateService.asmx/MakeCCDonation'),
-                              { 'PartnerID' => DONATENOW_AUTH['PartnerID'],
-                                'PartnerPW' => DONATENOW_AUTH['PartnerPW'],
-                                'PartnerSource' => DONATENOW_AUTH['PartnerSource'],
-                                'PartnerCampaign' => DONATENOW_AUTH['PartnerCampaign'],
-                                'NpoEin' => nil,
-                                'Designation' => nil,
-                                'Dedication' => nil,
-                                'DonorNpoDisclosure' => @donation.disclosure,
-                                'DonationAmount' => @donation.amount,
-                                'PartnerTransactionIdentifier' => nil,
-                                'DonorIpAddress' => request.remote_ip,
-                                'DonorFirstName' => @donor.first_name,
-                                'DonorLastName' => @donor.last_name,
-                                'DonorEmail' => @donor.email,
-                                'DonorAddress1' => @donor.address1,
-                                'DonorAddress2' => @donor.address2,
-                                'DonorCity' => @donor.city,
-                                'DonorState' => @donor.state,
-                                'DonorZip' => @donor.zip,
-                                'DonorPhone' => "#{@donor.phone_1}#{@donor.phone_2}#{@donor.phone_3}",
-                                'CardType' => @credit_card.card_type,
-                                'NameOnCard' => @credit_card.name,
-                                'CardNumber' => @credit_card.number,
-                                'ExpMonth' => @credit_card.expiry_date.month,
-                                'ExpYear' => @credit_card.expiry_date.year,
-                                'CSC' => @credit_card.csc
-                              })
+    url = URI.parse('https://qa4.networkforgood.org/PartnerDonationService/DonateService.asmx/MakeCCDonation')
+    req = Net::HTTP::Post.new(url.path)
+    req.set_form_data({ 'PartnerID' => DONATENOW_AUTH['PartnerID'],
+                        'PartnerPW' => DONATENOW_AUTH['PartnerPW'],
+                        'PartnerSource' => DONATENOW_AUTH['PartnerSource'],
+                        'PartnerCampaign' => DONATENOW_AUTH['PartnerCampaign'],
+                        'NpoEin' => nil,
+                        'Designation' => nil,
+                        'Dedication' => nil,
+                        'DonorNpoDisclosure' => @donation.disclosure,
+                        'DonationAmount' => @donation.amount,
+                        'PartnerTransactionIdentifier' => nil,
+                        'DonorIpAddress' => request.remote_ip,
+                        'DonorFirstName' => @donor.first_name,
+                        'DonorLastName' => @donor.last_name,
+                        'DonorEmail' => @donor.email,
+                        'DonorAddress1' => @donor.address1,
+                        'DonorAddress2' => @donor.address2,
+                        'DonorCity' => @donor.city,
+                        'DonorState' => @donor.state,
+                        'DonorZip' => @donor.zip,
+                        'DonorPhone' => "#{@donor.phone_1}#{@donor.phone_2}#{@donor.phone_3}",
+                        'CardType' => @credit_card.card_type,
+                        'NameOnCard' => @credit_card.name,
+                        'CardNumber' => @credit_card.number,
+                        'ExpMonth' => @credit_card.expiry_date.month,
+                        'ExpYear' => @credit_card.expiry_date.year,
+                        'CSC' => @credit_card.csc
+                      })
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = url.scheme == 'https'
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    res = http.start{|http| http.request(req)}
+
     puts "#{res.code} #{res.message}"
     puts res.body
     puts res.inspect
