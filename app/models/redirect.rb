26fc83ab6da8271b7e07a0762f6f3626cbdd3ef5
Jason Mott <jason@brattcollective.com>
2008-09-23 21:33:04 -0400

    made redirect model freeze when set from config file

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -1,13 +1,15 @@
 class Redirect < ActiveResource::Base
   # See config/environments for self.site value and @off value
   # self.site = "http://localhost/"
-  @off = true
+  # @off = true
   
   def self.off=(bool)
       @off = bool
+      self.dup.freeze
   end
   
   def self.off?
     @off
   end
+  
 end
