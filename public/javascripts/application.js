dbba73c6a17fff8a34c7c7ad646d57a1d539f330
Jason <jmott@blackhole>
2010-01-12 13:00:50 -0500

    added select/unselect all to form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function()
+		{
+			$("#select_all_sites").click(function()				
+			{
+             	//alert("got here " + this.checked)
+				var checked_status = this.checked;
+				$("input[@id=sites]").each(function()
+				{
+					//alert("got here " + this.value)
+					this.checked = checked_status;
+				});
+			});					
+		});
+		
